5ab586d0f6ed1f1be805cd62ace5dd4d7bc2c1b0
javirk <jgamtejero@gmail.com>
2021-12-16 12:16:24 +0100

    Try only classification. Add f1 score metric

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -103,7 +103,7 @@ def get_optimizer(p, parameters):
 
     elif p['optimizer'] == 'adam':
         try:
-            del p['momentum']
+            del p['optimizer_kwargs']['momentum']
         except KeyError:
             pass
         optimizer = torch.optim.Adam(parameters, **p['optimizer_kwargs'])
